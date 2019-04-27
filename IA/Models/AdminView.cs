fdd417e5b749a7b65de0dab5822dd39e1094aacd
AbdallahTarekRashad <41642236+AbdallahTarekRashad@users.noreply.github.com>
2019-04-27 10:33:57 -0700

    h

---
+++ IA/Models/AdminView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IA.Models
+{
+    public class AdminView
+    {
+        public users user = new users();
+        public List<users> AllUsers = new List<users>();
+        public List<projects> AllProjects = new List<projects>();
+    }
+}
