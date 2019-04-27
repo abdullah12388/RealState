716b7a1adc3f9943ff463f3753a8e168ebfd793b
5aligy <abdullah.mk96@yahoo.com>
2019-04-27 04:19:52 +0200

    add

---
+++ IA/Models/Qualifications.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace IA.Models
+{
+    public class Qualifications
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Q_Name { get; set; }
+        public string rate { get; set; }
+        [ForeignKey("User")]
+        public int userId { get; set; }
+        public virtual users User { get; set; }
+    }
+}
