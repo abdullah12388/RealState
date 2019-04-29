9398db509e2aeab85e3131e330c12aee603b6943
AbdallahTarekRashad <41642236+AbdallahTarekRashad@users.noreply.github.com>
2019-04-29 08:39:50 -0700

    4

---
+++ IA/Controllers/AdminController.cs
@@ -0,0 +1,125 @@
+﻿using IA.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IA.Controllers
+{
+    public class AdminController : Controller
+    {
+        private RealEstateContext db = new RealEstateContext();
+        // GET: Admin
+        public ActionResult Delete(int id)
+        {
+            var user = db.users.Single(x => x.Id == id);
+            if (user != null)
+            {
+                db.users.Remove(user);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index", "Profile");
+        }
+
+        public ActionResult DeleteView()
+        {
+            List<users> users = db.users.Where(x => x.userTypeId > 1).ToList();
+            return PartialView("DeleteUser",users);
+        }
+
+        public ActionResult Add(users usr, string type)
+        {
+            try
+            {
+                usr.userTypeId = int.Parse(type);
+
+
+                string FileName = Path.GetFileNameWithoutExtension(usr.photoFile.FileName);
+
+                //To Get File Extension  
+                string FileExtension = Path.GetExtension(usr.photoFile.FileName);
+
+                //Add Current Date To Attached File Name  
+                Random rnd = new Random();
+                int r = rnd.Next();
+                FileName = DateTime.Now.ToString("yyyyMMdd") + "-" + r.ToString() + FileName.Trim() + FileExtension;
+
+                string path = Path.Combine(Server.MapPath("~/Content/Images/"), FileName);
+                usr.photo = "Content/Images/" + FileName;
+
+
+                db.users.Add(usr);
+                db.SaveChanges();
+                usr.photoFile.SaveAs(path);
+                
+                return RedirectToAction("Index", "Profile");
+            }
+            catch (Exception)
+            {
+                ViewBag.User_Types = db.userType.Where(x => x.userTypeId > 1).ToList();
+                return View("Index" , "Profile");
+            }
+        }
+
+        public ActionResult AddView()
+        {
+            ViewBag.User_Types = db.userType.Where(x => x.userTypeId > 1).ToList();
+            users user = new users();
+            return PartialView("AddUser");
+        }
+
+        public ActionResult DeleteProject(int id)
+        {
+            var project = db.project.Single(x => x.Id == id);
+            if (project != null)
+            {
+                db.project.Remove(project);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index", "Profile");
+        }
+
+        public ActionResult ProjectView()
+        {
+            List<projects> projects = db.project.Where(x => x.Id > 0).ToList();
+            return PartialView("DeleteProject", projects);
+        }
+
+
+        public ActionResult AddProject(projects project,string id = "1")
+        {
+            project.customerId = int.Parse(id);
+            project.pStatus = 0;
+            db.project.Add(project);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Profile");
+        }
+
+        public ActionResult AddProjectView()
+        {
+            projects project = new projects();
+            ViewBag.cust = db.users.Where(x => x.userTypeId == 2).ToList();
+            return PartialView("AddProject", project);
+        }
+
+        public ActionResult UpdateProjectView(int id)
+        {
+            var project = db.project.SingleOrDefault(p => p.Id == id);
+            if (project == null){return HttpNotFound();}
+            return View("UpdateProject",project);
+        }
+
+        public ActionResult UpdateProject(projects project)
+        {
+            var projectindb = db.project.Single(p => p.Id == project.Id);
+            projectindb.pName = project.pName;
+            projectindb.pSalary = project.pSalary;
+            projectindb.pArea = project.pArea;
+            projectindb.pDescription = project.pDescription;
+            db.SaveChanges();
+            return RedirectToAction("Index", "Profile");
+        }
+    }
+}
--- IA/Controllers/ProjectManagerController.cs
+++ IA/Controllers/ProjectManagerController.cs
@@ -1,7 +1,9 @@
 ﻿using IA.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,21 +13,13 @@ namespace IA.Controllers
     {
         // GET: ProjectManager
         private RealEstateContext db = new RealEstateContext();
-
-        
-        
-        public ActionResult pre_project()
+        public ActionResult comment()
         {
-            int ID = Convert.ToInt32(Session["ID"]);
-            //ViewBag.proj = db.project.Where(x => x.pmId == ID && x.progress == 2).ToList();
-            ViewBag.c_proj = db.project.Where(x => x.pmId == ID && x.progress == 1).ToList();
-            return PartialView("prevProj");
+            ViewBag.Id = new SelectList(db.project, "Id", "pName");
+            Comments com = new Comments();
+            return PartialView("_comment", com);
         }
 
-        public ActionResult Search_team()
-        {
-            return PartialView();
-        }
         //ended
         public ActionResult Exp_chart()
         {
@@ -33,13 +27,65 @@ namespace IA.Controllers
             ViewBag.Exp = db.Experience.Where(x => x.userId == ID).ToList();
             return PartialView("chart");
         }
-        public ActionResult PM_exp()
+        public ActionResult cur_projects()
         {
             int ID = Convert.ToInt32(Session["ID"]);
-            //ViewBag.Exp = db.Experience.Where(x => x.userId == ID).ToList();
             ViewBag.proj = db.project.Where(x => x.pmId == ID && x.progress == 2).ToList();
-            //ViewBag.c_proj = db.project.Where(x => x.pmId == ID && x.progress == 1).ToList();
-            return PartialView("PM_Exp");
+            return PartialView("curProj");
         }
+        public ActionResult pre_project()
+        {
+            int ID = Convert.ToInt32(Session["ID"]);
+            ViewBag.c_proj = db.project.Where(x => x.pmId == ID && x.progress == 3).ToList();
+            return PartialView("prevProj");
+        }
+        public ActionResult cur_projects_list()
+        {
+            int ID = Convert.ToInt32(Session["ID"]);
+            var project = db.project.Where(p => p.pmId == ID).Include(p => p.Customer).Include(p => p.Pm).Include(p => p.Team);
+            return PartialView("_Control_project", project.ToList());
+        }
+        public ActionResult Edit(int id)
+        {
+            projects projects = db.project.Find(id);
+            if (projects == null) { return HttpNotFound(); }
+            ViewBag.customerId = new SelectList(db.users, "Id", "firstName", projects.customerId);
+            ViewBag.pmId = new SelectList(db.users, "Id", "firstName", projects.pmId);
+            ViewBag.pTeam = new SelectList(db.team, "Id", "Id", projects.pTeam);
+            return PartialView("_EditProj", projects);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,pName,pSalary,pDescription,pArea,pmId,customerId,pTeam")] projects projects, string statue)
+        {
+            if (ModelState.IsValid)
+            {
+                projects.progress = int.Parse(statue);
+                db.Entry(projects).State = EntityState.Modified;
+                projects.pmId = Convert.ToInt32(Session["ID"]);
+                db.SaveChanges();
+                Session["Valid_edit"] = 0;
+                return RedirectToAction("Index", "Profile");
+            }
+            ViewBag.customerId = new SelectList(db.users, "Id", "firstName", projects.customerId);
+            ViewBag.pmId = new SelectList(db.users, "Id", "firstName", projects.pmId);
+            ViewBag.pTeam = new SelectList(db.team, "Id", "Id", projects.pTeam);
+            Session["Valid_edit"] = 1;
+            return RedirectToAction("Index", "Profile");
+        }
+        public ActionResult cancel(int? id)
+        {
+            //if (id == null){ return HttpNotFound(); }
+            projects project = db.project.Find(id);
+            //if (project == null){return HttpNotFound();}
+            project.pmId = null;
+            project.pStatus = 1;
+            Session["Valid_edit"] = 0;
+            db.SaveChanges();
+            int ID = Convert.ToInt32(Session["ID"]);
+            var projects = db.project.Where(p => p.pmId == ID).Include(p => p.Customer).Include(p => p.Pm).Include(p => p.Team);
+            return PartialView("_Control_project", projects.ToList());
+        }
+
     }
 }
